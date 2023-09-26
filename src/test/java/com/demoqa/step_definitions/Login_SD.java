457c3d928791f4c12c57fd82a479cb70ce1ac647
Burak Yilmaz <burakyasinyilmaz87@gmail.com>
2023-09-27 01:34:45 +0300

    Login_SD class are created

---
+++ src/test/java/com/demoqa/step_definitions/Login_SD.java
@@ -0,0 +1,30 @@
+package com.demoqa.step_definitions;
+
+import io.cucumber.java.en.*;
+
+public class Login_SD {
+
+    @Given("user is on the login page")
+    public void user_is_on_the_login_page() {
+
+
+
+    }
+    @When("user enters a VALID username {string} into the username input box")
+    public void user_enters_a_valıd_username_into_the_username_input_box(String string) {
+
+    }
+    @When("user enters a VALID password {string} into the password input box")
+    public void user_enters_a_valıd_password_into_the_password_input_box(String string) {
+
+    }
+    @When("the user clicks on the Log in button")
+    public void the_user_clicks_on_the_log_in_button() {
+
+    }
+    @Then("the user should land on the profile page")
+    public void the_user_should_land_on_the_profile_page() {
+
+    }
+
+}
