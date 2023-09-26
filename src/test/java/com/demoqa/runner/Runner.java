72e83e14c82ac66245f1b30cea63b51b0c2990e1
Burak Yilmaz <burakyasinyilmaz87@gmail.com>
2023-09-27 00:52:45 +0300

    Runner and FailedTestRunner are added

---
+++ src/test/java/com/demoqa/runner/Runner.java
@@ -0,0 +1,28 @@
+package com.demoqa.runner;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        plugin = {
+                "json:target/cucumber.json",
+                "html:target/cucumber-report.html",
+
+                "rerun: target/rerun.txt"},
+        features = "src/test/resources",
+        glue = "com/demoqa/step_definitions",
+        dryRun = true,
+        tags = ""
+
+
+
+
+)
+
+public class Runner {
+
+}
